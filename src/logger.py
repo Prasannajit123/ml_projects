bed88516bb88769ecef11fe3b150afb216d2c4a4
titan <prasannajitlenka504@gmail.com>
2023-12-09 20:13:50 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,13 @@
+import logging
+import os
+from datetime import datetime
+# Create and configure logger
+logfile=f"{datetime.now().strftime('%Y-%m-%d-%H-%M-S')}.log"
+log_path=os.path.join(os.getcwd(),'logs',logfile)
+os.makedirs(log_path,exist_ok=True)
+
+log_file_path=os.path.join(log_path,logfile)
+logging.basicConfig(filename=log_file_path,
+                    format='[%(asctime)s] %(lineno)d  %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
